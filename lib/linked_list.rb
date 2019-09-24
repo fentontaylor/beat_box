f213e10252aa77da8d1c915a346cf29513cca010
Fenton Taylor <fentontaylor@gmail.com>
2019-09-24 10:40:55 -0600

    Add #append with some private helpers

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -8,4 +8,27 @@ class LinkedList
   def empty?
     @head.nil?
   end
+
+  def last_node(node)
+    return node if node.tail?
+    last_node(node.next_node)
+  end
+
+  def append(data)
+    empty? ? set_head(data) : set_tail(data)
+  end
+
+  private
+
+  def new_node(data)
+    Node.new(data)
+  end
+
+  def set_head(data)
+    @head = new_node(data)
+  end
+
+  def set_tail(data)
+    last_node(@head).next_node = new_node(data)
+  end
 end
--- test/linked_list_test.rb
+++ test/linked_list_test.rb
@@ -23,4 +23,24 @@ class LinkedListTest < Minitest::Test
 
     refute @list.empty?
   end
+
+  def test_last_node
+    node_1 = Node.new('first')
+    node_2 = Node.new('tail')
+    @list.head = node_1
+    @list.head.next_node = node_2
+
+    assert_equal node_2, @list.last_node(@list.head)
+  end
+
+  def test_append
+    @list.append('doop')
+    assert_instance_of Node, @list.head
+    assert_equal 'doop', @list.head.data
+    assert_nil @list.head.next_node
+
+    @list.append('beep')
+    assert_instance_of Node, @list.head.next_node
+    assert_equal 'beep', @list.head.next_node.data
+  end
 end
