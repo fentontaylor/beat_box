99504eba16e5e817a59c6384519e59ccadab2f79
Fenton Taylor <fentontaylor@gmail.com>
2019-09-24 11:59:06 -0600

    Add #append and #count wrapper methods

--- lib/beat_box.rb
+++ lib/beat_box.rb
@@ -4,4 +4,13 @@ class BeatBox
   def initialize
     @list = LinkedList.new
   end
+
+  def append(string)
+    strings = string.split
+    strings.each { |str| @list.append(str) }
+  end
+
+  def count
+    @list.count
+  end
 end
--- test/beat_box_test.rb
+++ test/beat_box_test.rb
@@ -3,6 +3,7 @@ require 'minitest/pride'
 require './lib/node'
 require './lib/linked_list'
 require './lib/beat_box'
+require 'pry'
 
 class BeatBoxTest < Minitest::Test
   def setup
@@ -16,5 +17,22 @@ class BeatBoxTest < Minitest::Test
   def test_attributes
     assert_instance_of LinkedList, @beatbox.list
     assert_nil @beatbox.list.head
+  end
+
+  def test_append
+    @beatbox.append('shoop dee doo')
+
+    assert_equal 'shoop', @beatbox.list.head.data
+    assert_equal 'dee', @beatbox.list.head.next_node.data
+  end
+
+  def test_count
+    assert_equal 0, @beatbox.count
+
+    @beatbox.append("deep doo ditt")
+    assert_equal 3, @beatbox.count
+    
+    @beatbox.append("boom dee pow")
+    assert_equal 6, @beatbox.count
   end
 end
